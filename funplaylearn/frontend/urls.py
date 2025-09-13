bf5bfcd833261ab1a5a13fd9ef61e430a134c7f5
FPL-star <official@funplaylearn.org>
2025-09-13 16:40:56 +0545

    add static pages to the website
    pages include
    -home
    -about us
    -resources
    without any people pictures

---
+++ funplaylearn/frontend/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("about_us", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
+    path("resources", TemplateView.as_view(template_name="resources.html"), name="resources"),
+]
