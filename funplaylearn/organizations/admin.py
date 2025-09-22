684e217758b3f494b7cabc27727bc2086bc5be55
adnanvaldes <github@terminus.earth>
2025-09-22 19:37:17 +0200

    add organization migration to seed FPL data; disable a few reference models

--- funplaylearn/organizations/admin.py
+++ funplaylearn/organizations/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from organizations.models import Organization, Role
-from core.models import OrganizationContact
 
 
 admin.site.register(Organization)
 admin.site.register(Role)
-admin.site.register(OrganizationContact)
